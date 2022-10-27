6926e422dc92162d07f46f57537adc0a63c32c5d
mayank1411 <mayank030mink@gmail.com>
2022-10-27 14:27:36 +0530

    HideAndSeek

---
+++ easy/scriptEasy.js
@@ -0,0 +1,106 @@
+var address = ["A0","A1","A2",
+               "B0","B1","B2",
+               "C0","C1","C2"]
+let WON = false;
+var rating;
+var iDanger = Math.floor(Math.random()*address.length) // random index of danger
+var addDanger = address[iDanger];
+console.log(addDanger);
+address.splice(iDanger,1); // remove the danger address
+console.log(address);
+var chances = 0;
+var numGift = 3;
+// var dangerCard = document.getElementById(addDanger);
+// console.log(dangerCard);
+var giftAddress = [];
+
+function checkGift(numGift){
+   if(numGift == 0){
+    WON = true;
+    rating = Math.round((3/chances) * 5); // calculates rating (1-5)
+    console.log(rating);
+    setTimeout(getResult,1000);
+   }
+}
+
+for(var i =0; i<3; i++){
+    var iGift = Math.floor(Math.random()*address.length);
+    giftAddress[i] = address[iGift];
+    console.log(giftAddress);
+    address.splice(iGift,1); // remove the gifts address
+}
+console.log(giftAddress);
+console.log(address);
+var getCard = () => {
+    var guessCell = document.getElementById("idGuess"); // C2
+    console.log(guessCell);
+    if(guessCell.value == addDanger){  // C2 == C2
+    var dangerCard = document
+    .getElementById(addDanger) // C2 cell 
+      .querySelector('.DANGER') 
+     console.log(dangerCard);
+    dangerCard.style.opacity = "0.99";
+    guessCell.value = "";
+    setTimeout(getResult,1000) ;
+   
+ }
+ for(var i=0; i<3; i++){
+    if(guessCell.value == giftAddress[i]){
+        var giftCard = document.getElementById(giftAddress[i]).querySelector(".GIFT");
+            // console.log(giftCard);
+            giftCard.style.opacity = "0.99";
+            guessCell.value = "";
+            chances++;
+            numGift--;
+            checkGift(numGift)
+            break;
+    }
+ }
+ for(var i=0; i<5; i++){
+    if(guessCell.value == address[i]){
+        var missCard = document.getElementById(address[i]).querySelector(".MISS");
+            // console.log(missCard);
+            missCard.style.opacity = "0.99";
+            guessCell.value = "";
+            chances++
+            break;
+ }
+}
+}
+console.log(chances);
+
+
+
+        //MODAL RESULT
+function getResult(){
+    if(WON == false){
+        document.getElementById("result").innerHTML = "YOU LOSE!!!!!!!"
+    }
+    else{
+        document.getElementById("result").innerHTML = "YOU WON!!!!!!!";
+         for(var j=1; j<=rating; j++){
+            var s = document.getElementById(j).querySelector(".Star")
+            s.style.opacity = "0.99";
+         }
+           
+        
+        
+    }
+     // Get the modal
+document.getElementById("myModal").style.display = "block";
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    document.getElementById("myModal").style.display = "none";
+    location.reload();
+}
+    }
+
+
+
+
+
